d8b82761f84d42e7af3ac4e1fe057b53099c9029
Sky Blue <dawadiaakash@gmail.com>
2024-06-26 14:28:41 +0545

    Toggle Moon/Sun

--- src/script.js
+++ src/script.js
@@ -10,3 +10,16 @@ function hidesidebar(){
     sidebar.style.display = 'none'
     
 }
+
+function darkMode() {
+    const body = document.body;
+    const moonIcon = document.querySelector('.moon');
+
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        moonIcon.src = '/img/sun.svg'; 
+    } else {
+        moonIcon.src = '/img/moon.svg'; 
+    }
+}
